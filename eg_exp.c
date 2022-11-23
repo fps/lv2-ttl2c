ed697a5bca1bb7976a0c13d0b5fde123b02c49a8
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-11-23 11:03:31 +0100

    use cleanup hook

--- eg_exp.c
+++ eg_exp.c
@@ -6,18 +6,24 @@
 struct eg_exp_state 
 {
     float s;
+    float sampling_interval;
 };
 
 struct eg_exp* instantiate(struct eg_exp *instance, double sample_rate, const char *bundle_path, const LV2_Feature *const *features)
 {
     instance->state = malloc(sizeof(struct eg_exp_state));
     memset(instance->state, 0, sizeof(struct eg_exp_state));
+    instance->state->sampling_interval = 1.0f / sample_rate;
     return instance;
+}
+
+void cleanup(struct eg_exp *instance) {
+    free(instance->state);
 }
 
 void run(struct eg_exp *instance, uint32_t nframes, float *t1, float *in, float *out)
 {
-    const float a = expf(1/t1[0]);
+    const float a = 1.0f - expf(-instance->state->sampling_interval/t1[0]);
     for (uint32_t frame = 0; frame < nframes; ++frame)
     {
         out[frame] = in[frame] * a + instance->state->s * (1 - a);
@@ -29,6 +35,7 @@ struct eg_exp_callbacks_t eg_exp_callbacks =
 {
     .run = run,
     .instantiate = instantiate,
+    .cleanup = cleanup,
 };
 
 #include "ttl2c_eg_exp.c"
