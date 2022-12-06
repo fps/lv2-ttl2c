34777f461855196f80eb8d0a984ffb20ef7912c8
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-12-06 10:17:32 +0100

    install templates

---
+++ lv2-ttl2c/templates/plugin.h
@@ -0,0 +1,44 @@
+#ifndef plugin_cb_hh
+#define plugin_cb_hh
+
+#include <lv2.h>
+#include <lv2/log/logger.h>
+#include <lv2/core/lv2_util.h>
+#include <stdint.h>
+#include <lv2/atom/atom.h>
+#include <lv2/atom/util.h>
+#include <lv2/midi/midi.h>
+typedef struct plugin_state plugin_state_t;
+
+typedef struct {
+    plugin_state_t *state;
+    void *ports[3];
+    LV2_URID_Map *map;
+    LV2_URID midi_MidiEvent;
+    LV2_Log_Logger logger;
+} plugin_t;
+
+
+typedef struct {
+    LV2_Atom_Sequence const * const data;
+} plugin_port_control_t;
+
+typedef struct {
+    float const * const data;
+} plugin_port_in_t;
+
+typedef struct {
+    float * const data;
+} plugin_port_out_t;
+
+typedef struct {
+    plugin_t* (*const instantiate)(plugin_t *instance, double sample_rate, const char *bundle_path, const LV2_Feature *const *features);
+    void (*const connect_port)(plugin_t *instance, uint32_t port, void *data_location);
+    void (*const activate)(plugin_t *instance);
+    void (*const run)(plugin_t *instance, uint32_t sample_count, const plugin_port_control_t control, const plugin_port_in_t in, const plugin_port_out_t out);
+    void (*const deactivate)(plugin_t *instance);
+    void (*const cleanup)(plugin_t *instance);
+    const void *(*const extension_data)(const char *uri);
+} plugin_callbacks_t;
+
+#endif    
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name = 'lv2-ttl2c',
     version = '1.0.2',
-    scripts = [ 'lv2-ttl2c' ],
-    packages = [ ],
+    scripts = [ 'lv2-ttl2c/bin/lv2-ttl2c' ],
+    packages = [ 'lv2-ttl2c' ],
     include_package_data = True,
     install_requires = [ 'jinja2' ],
 )
