81c0be5c1674d78c5e3e43a20084bc06fcfbe090
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-05-01 21:42:04 +0200

    reformat

--- lv2_ttl2c/templates/plugin.c
+++ lv2_ttl2c/templates/plugin.c
@@ -1,22 +1,29 @@
 #ifndef ttl2c_plugin_cc
 #define ttl2c_plugin_cc
     
-static void plugin_connect_port_desc (LV2_Handle instance, uint32_t port, void *data_location) {
+static void plugin_connect_port_desc (LV2_Handle instance, uint32_t port, void *data_location)
+{
     plugin_t *tinstance = (plugin_t*) instance;
 
-    if (plugin_callbacks.connect_port) {
+    if (plugin_callbacks.connect_port) 
+    {
         plugin_callbacks.connect_port(tinstance, port, data_location);
-    } else {
-        if (port < {{ ports | length }}) {
+    } 
+    else 
+    {
+        if (port < {{ ports | length }}) 
+        {
             (tinstance)->ports[port] = data_location;
         }
     }
 }
 
-static LV2_Handle plugin_instantiate_desc (const LV2_Descriptor *descriptor, double sample_rate, const char *bundle_path, const LV2_Feature *const *features) {
+static LV2_Handle plugin_instantiate_desc (const LV2_Descriptor *descriptor, double sample_rate, const char *bundle_path, const LV2_Feature *const *features) 
+{
     plugin_t *instance = (plugin_t*)malloc(sizeof(plugin_t));
 
-    if (!instance) {
+    if (!instance) 
+    {
         return NULL;
     }
 
@@ -26,48 +33,58 @@ static LV2_Handle plugin_instantiate_desc (const LV2_Descriptor *descriptor, dou
 
 {% if needs_urid_map %}
     const char* missing =  lv2_features_query (features, LV2_URID__map, &instance->map, true, NULL);
-    if (missing) {
+    if (missing) 
+    {
         lv2_log_note(&instance->logger, "Missing feature: %s\n", missing);
         free(instance);
         return NULL;
     }
 {% endif %}
 
-    if (plugin_callbacks.instantiate) {
+    if (plugin_callbacks.instantiate) 
+    {
         instance = plugin_callbacks.instantiate (instance, sample_rate, bundle_path, features);
     }
 
     return (LV2_Handle)(instance);
 }
 
-static void plugin_cleanup_desc (LV2_Handle instance) {
+static void plugin_cleanup_desc (LV2_Handle instance) 
+{
     plugin_t *tinstance = (plugin_t*) instance;
 
-    if (plugin_callbacks.cleanup) {
+    if (plugin_callbacks.cleanup) 
+    {
         plugin_callbacks.cleanup (tinstance);
     }
 
     free(tinstance);
 }
 
-static void plugin_activate_desc (LV2_Handle instance) {
+static void plugin_activate_desc (LV2_Handle instance) 
+{
     plugin_t *tinstance = (plugin_t*) instance;
 
-    if (plugin_callbacks.activate) {
+    if (plugin_callbacks.activate) 
+    {
         plugin_callbacks.activate (tinstance);
     }
 }
 
-static void plugin_deactivate_desc (LV2_Handle instance) {
+static void plugin_deactivate_desc (LV2_Handle instance) 
+{
     plugin_t *tinstance = (plugin_t*) instance;
 
-    if (plugin_callbacks.deactivate) {
+    if (plugin_callbacks.deactivate) 
+    {
         plugin_callbacks.deactivate (tinstance);
     }
 }
 
-static void plugin_run_desc (LV2_Handle instance, uint32_t sample_count) {
-    if (plugin_callbacks.run) {
+static void plugin_run_desc (LV2_Handle instance, uint32_t sample_count) 
+{
+    if (plugin_callbacks.run) 
+    {
         plugin_t *tinstance = (plugin_t*) instance;
 
 {% for port in ports %}
@@ -78,15 +95,20 @@ static void plugin_run_desc (LV2_Handle instance, uint32_t sample_count) {
     }
 }
 
-static const void *plugin_extension_data_desc (const char *uri) {
-    if (plugin_callbacks.extension_data) {
+static const void *plugin_extension_data_desc (const char *uri) 
+{
+    if (plugin_callbacks.extension_data) 
+    {
         return plugin_callbacks.extension_data (uri);
-    } else {
+    } 
+    else 
+    {
         return 0;
     }
 }
 
-static LV2_Descriptor plugin_descriptor = {
+static LV2_Descriptor plugin_descriptor = 
+{
     "{{ uri }}",
     plugin_instantiate_desc,
     plugin_connect_port_desc,
@@ -97,10 +119,14 @@ static LV2_Descriptor plugin_descriptor = {
     plugin_extension_data_desc
 };
 
-LV2_SYMBOL_EXPORT const LV2_Descriptor* lv2_descriptor (uint32_t index) {
-    if (0 == index) {
+LV2_SYMBOL_EXPORT const LV2_Descriptor* lv2_descriptor (uint32_t index) 
+{
+    if (0 == index) 
+    {
           return &plugin_descriptor;
-    } else { 
+    } 
+    else 
+    { 
           return NULL;
     }
 }
